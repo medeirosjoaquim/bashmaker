e77fce55ed157cded9bba92b1fdbae17e0859a2a
johnbox <johnboxcodes@example.com>
2017-12-08 19:57:19 -0200

    variable in snprintf

--- makercreator.c
+++ makercreator.c
@@ -5,6 +5,29 @@ void generateMaker(){
   FILE *fptr;
   char name[100];
 
+  char script[512];
+  snprintf(script, sizeof script, "%s", 
+
+      "#!/bin/bash\n"
+      "function getStat(){\n"
+      "  declare -a ARRAY\n"
+      "  count=0;\n"
+      "  for file in *.cpp\n"
+      "  do\n"
+      "    ARRAY[$count]=$file;\n"
+      "    ((count++));\n"
+      "  done\n"
+      "  for((i=0;i<${#ARRAY[@]};i++)){\n"
+      "    printf \"%s -- \" \"${ARRAY[$i]}\";\n"
+      "    stat -c %Z ${ARRAY[$i]};\n"
+      "  }\n"
+      "}\n"
+      "while :\n"
+      "do\n"
+      "  getStat;\n"
+      "  sleep 2;\n"
+      "  clear\n"
+      "done\n");
   fptr = fopen("program.txt", "w");
   if(fptr == NULL) {
     printf("Error!");
@@ -12,9 +35,9 @@ void generateMaker(){
   }
 
   printf("Enter a sentence:\n");
-   fgets(name,100,stdin);
+  fgets(name,100,stdin);
 
- fprintf(fptr,"%s", name);
+  fprintf(fptr,"%s%s", name,script);
   fclose(fptr);
 }
 
