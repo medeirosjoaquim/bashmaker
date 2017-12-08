67cda56db894ff1d48d7197c45ada4e49979aa1c
johnbox <johnboxcodes@example.com>
2017-12-08 19:19:33 -0200

    first branch commit

---
+++ makercreator.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+#include <stdlib.h>
+
+void generateMaker(){
+  char sentence[1000];
+   FILE *fptr;
+
+   fptr = fopen("program.txt", "w");
+   if(fptr == NULL)
+   {
+      printf("Error!");
+      exit(1);
+   }
+   
+   printf("Enter a sentence:\n");
+  gets(sentence);
+
+   fprintf(fptr,"%s", sentence);
+   fclose(fptr);
+}
+
+int main(){
+generateMaker();
+  }
